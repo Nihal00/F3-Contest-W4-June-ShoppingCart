fbfd97a00d747d5e7a9caf34315fbc1c632224a3
Nihal00 <nihallsuvarna@gmail.com>
2023-06-29 18:44:36 +0530

    search-selection filter functionality

--- src/shop.js
+++ src/shop.js
@@ -1,10 +1,11 @@
 //DOM element
-const searchBar = document.querySelector(".search-bar");
+const searchBar = document.getElementById("search-bar");
 const productDisplay = document.getElementById("display-product");
 const itemSelector = document.querySelector(".search-selector");
 const itemSelectorList = document.querySelectorAll(
   ".search-selector .item-selector"
 );
+
 let productData = [];
 
 async function getProductDetails() {
@@ -21,43 +22,74 @@ async function getProductDetails() {
 async function displayProducts() {
   productDisplay.innerHTML = "";
   productData.forEach((product) => {
-    productDisplay.innerHTML += `
-        <div class="item-container">
-        <div class="inner-item-container">
-        <div class="img-container">
-        <img
-          class="item-img"
-          src="${product.image}"
-          alt="${product.description}"
-          
-        />
-      </div>
-          <div class="item-details">
-            <div class="price-size">
-              <p class="item-price">$${product.price}</p>
-              <p class="item-size">${product.sizes}</p>
-            </div>
-            <div class="items-colors">Colors :
-              <span class="circle">
-                <span class="material-symbols-outlined circles"> circle </span>
-                <span class="material-symbols-outlined circles"> circle </span>
-                <span class="material-symbols-outlined circles"> circle </span>
-              </span>
-            </div>
-            <div class="items-rating">Rating :
-              <span class="star">
-                <span class="material-symbols-outlined"> grade </span>
-                <span class="material-symbols-outlined"> grade </span>
-                <span class="material-symbols-outlined"> grade </span>
-                <span class="material-symbols-outlined"> grade </span>
-                <span class="material-symbols-outlined"> grade </span>
-              </span>
-            </div>
-          </div>
-        </div>
-        <button id="add-to-cart" type="submit">Add to Cart</button>
-      </div>
-        `;
+    let itemContainer = document.createElement("div");
+    let innerItemContainer = document.createElement("div");
+    let imgContainer = document.createElement("div");
+    let itemDetails = document.createElement("div");
+    let priceSize = document.createElement("div");
+    let price = document.createElement("p");
+    let size = document.createElement("p");
+    let itemsColors = document.createElement("div");
+    let colorCircle = document.createElement("span");
+    let itemsRating = document.createElement("div");
+    let star = document.createElement("span");
+    let img = document.createElement("img");
+    let btn = document.createElement("button");
+
+    itemContainer.className = "item-container";
+    innerItemContainer.className = "inner-item-container";
+    imgContainer.className = "img-container";
+    img.className = "item-img";
+    img.src = product.image;
+    itemDetails.className = "item-details";
+    priceSize.className = "price-size";
+    price.className = "item-price";
+    size.className = "item-size";
+    price.innerText = `$${product.price}`;
+    size.innerText = product.sizes;
+    itemsColors.className="items-colors";
+    itemsColors.innerText = "Color :";
+    colorCircle.className = 'circle';
+
+    // circle color
+    for(let i = 0; i < product.colors.length; i++){
+      let colorType = document.createElement("span");
+      colorType.className = "material-icons-outlined";
+      colorType.innerText = "lens";
+      colorType.style.color = product.colors[i];
+      colorCircle.appendChild(colorType);
+    }
+    itemsRating.className = 'items-rating';
+    itemsRating.innerText = 'Rating :';
+    star.className = "star";
+    
+    //star coloring
+    for(let i = 0; i < Math.round(product.rating.rate); i++){
+      let ratingStar = document.createElement("span");
+      ratingStar.className = "material-icons-outlined";
+      ratingStar.innerText = "star";
+      star.appendChild(ratingStar);
+    }
+
+    btn.className = 'add-to-cart';
+    btn.type = 'submit';
+    btn.innerText = 'Add to Cart';
+    
+    itemsColors.appendChild(colorCircle);
+    itemsRating.appendChild(star);
+    priceSize.appendChild(price);
+    priceSize.appendChild(size);
+    itemDetails.appendChild(priceSize);
+    itemDetails.appendChild(itemsColors);
+    itemDetails.appendChild(itemsRating);
+    imgContainer.appendChild(img);
+    innerItemContainer.appendChild(imgContainer);
+
+    innerItemContainer.appendChild(itemDetails);
+    itemContainer.appendChild(innerItemContainer);
+    itemContainer.appendChild(btn);
+
+    productDisplay.append(itemContainer)
   });
 }
 
@@ -66,52 +98,83 @@ function colorFill() {
   console.log(colorsfilled);
   //  return `<span style='background:${colorsfilled[0]}'>${colorsfilled[1]}</
   for (let i = 0; i < colorsfilled.length; i++) {
-    span.append (`<span class="material-symbols-outlined" style="backgorundColor: ${colorsfilled[i]}">grade</span>`);
+    span.append(
+      `<span class="material-symbols-outlined" style="backgorundColor: ${colorsfilled[i]}">grade</span>`
+    );
   }
-
 }
 
 async function displaySortedProduct(list) {
   productDisplay.innerHTML = "";
   list.forEach((product) => {
-    productDisplay.innerHTML += `
-        <div class="item-container">
-        <div class="inner-item-container">
-        <div class="img-container">
-        <img
-          class="item-img"
-          src="${product.image}"
-          alt="${product.description}"
-          
-        />
-      </div>
-          <div class="item-details">
-            <div class="price-size">
-              <p class="item-price">$${product.price}</p>
-              <p class="item-size">${product.sizes}</p>
-            </div>
-            <div class="items-colors">Colors :
-            <span class="circle">
-              <span class="material-symbols-outlined circles"> circle </span>
-              <span class="material-symbols-outlined circles"> circle </span>
-              <span class="material-symbols-outlined circles"> circle </span>
-            </span>
-            </div>
-            <div class="items-rating">Rating :
-            <span class="star">
-              <span class="material-symbols-outlined"> grade </span>
-              <span class="material-symbols-outlined"> grade </span>
-              <span class="material-symbols-outlined"> grade </span>
-              <span class="material-symbols-outlined"> grade </span>
-              <span class="material-symbols-outlined"> grade </span>
-            </span>
-            </div>
-            
-          </div>
-        </div>
-        <button id="add-to-cart" type="submit">Add to Cart</button>
-      </div>
-        `;
+    let itemContainer = document.createElement("div");
+    let innerItemContainer = document.createElement("div");
+    let imgContainer = document.createElement("div");
+    let itemDetails = document.createElement("div");
+    let priceSize = document.createElement("div");
+    let price = document.createElement("p");
+    let size = document.createElement("p");
+    let itemsColors = document.createElement("div");
+    let colorCircle = document.createElement("span");
+    let itemsRating = document.createElement("div");
+    let star = document.createElement("span");
+    let img = document.createElement("img");
+    let btn = document.createElement("button");
+
+    itemContainer.className = "item-container";
+    innerItemContainer.className = "inner-item-container";
+    imgContainer.className = "img-container";
+    img.className = "item-img";
+    img.src = product.image;
+    itemDetails.className = "item-details";
+    priceSize.className = "price-size";
+    price.className = "item-price";
+    size.className = "item-size";
+    price.innerText = `$${product.price}`;
+    size.innerText = product.sizes;
+    itemsColors.className="items-colors";
+    itemsColors.innerText = "Color :";
+    colorCircle.className = 'circle';
+
+    // circle color
+    for(let i = 0; i < product.colors.length; i++){
+      let colorType = document.createElement("span");
+      colorType.className = "material-icons-outlined";
+      colorType.innerText = "lens";
+      colorType.style.color = product.colors[i];
+      colorCircle.appendChild(colorType);
+    }
+    itemsRating.className = 'items-rating';
+    itemsRating.innerText = 'Rating :';
+    star.className = "star";
+    
+    //star coloring
+    for(let i = 0; i < Math.round(product.rating.rate); i++){
+      let ratingStar = document.createElement("span");
+      ratingStar.className = "material-icons-outlined";
+      ratingStar.innerText = "star";
+      star.appendChild(ratingStar);
+    }
+
+    btn.className = 'add-to-cart';
+    btn.type = 'submit';
+    btn.innerText = 'Add to Cart';
+    
+    itemsColors.appendChild(colorCircle);
+    itemsRating.appendChild(star);
+    priceSize.appendChild(price);
+    priceSize.appendChild(size);
+    itemDetails.appendChild(priceSize);
+    itemDetails.appendChild(itemsColors);
+    itemDetails.appendChild(itemsRating);
+    imgContainer.appendChild(img);
+    innerItemContainer.appendChild(imgContainer);
+
+    innerItemContainer.appendChild(itemDetails);
+    itemContainer.appendChild(innerItemContainer);
+    itemContainer.appendChild(btn);
+
+    productDisplay.append(itemContainer)
   });
 }
 
@@ -165,6 +228,7 @@ async function sortedProduct(typeOfProduct) {
   }
 }
 
+
 itemSelectorList.forEach((selector) => {
   selector.addEventListener("click", async (e) => {
     itemSelector.querySelector(".active").classList.remove("active");
@@ -179,6 +243,25 @@ itemSelectorList.forEach((selector) => {
     }
   });
 });
+
+function searchInput(inputs){
+  const filterArr = [];
+  for (const data of productData) {
+    if (data.title.includes(inputs)) {
+      filterArr.push(data);
+    }
+  }
+  return filterArr;
+}
+
+
+
+searchBar.addEventListener('input', async (e) => {
+    console.log(e.target.value);
+    let searchResult = searchInput(e.target.value);
+    await displaySortedProduct(searchResult);
+})
+
 
 window.addEventListener("load", async () => {
   productData = await getProductDetails();
